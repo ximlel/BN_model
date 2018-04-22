2b206581c2c7f5ea43c792d5eeeb7c80ef9c26db
ximlel <qq352109085@gmail.com>
2018-04-22 12:14:58 +0800

    add d_x*

--- src/quasilinear.m
+++ src/quasilinear.m
@@ -3,6 +3,11 @@ function QUA = quasilinear(lo,u,p,p_g,u_s,s_or_g,A_or_C)
 QUA = zeros(4,4);
 global gama_s gama_g p0;
 QUA(2,3)=1.0;
+if strcmp(A_or_C,'C')==1
+    QUA(1,1)=u_s;
+elseif strcmp(A_or_C,'A')~=1
+    error('NOT A(U) or C(U)!');
+end
 if strcmp(s_or_g,'g')==1 %gas phase
     E=p/(gama_g-1)+0.5*lo*u^2;
     H=(E+p)/lo;
@@ -14,11 +19,8 @@ if strcmp(s_or_g,'g')==1 %gas phase
     QUA(4,3)=H-(gama_g-1)*u^2;
     QUA(4,4)=gama_g*u;
     if strcmp(A_or_C,'C')==1
-        QUA(1,1)=u_s;
         QUA(3,1)=QUA(3,1)+p_g;
         QUA(4,1)=QUA(4,1)+p_g*u_s;
-    elseif strcmp(A_or_C,'A')~=1
-        error('NOT A(U) or C(U)!');
     end
 elseif strcmp(s_or_g,'s')==1%solid phase
     QUA(3,1)=-gama_s*p0;
@@ -33,11 +35,8 @@ elseif strcmp(s_or_g,'s')==1%solid phase
     QUA(4,3)=H-(gama_s-1)*u^2;
     QUA(4,4)=gama_s*u;
     if strcmp(A_or_C,'C')==1
-        QUA(1,1)=u_s;
         QUA(3,1)=QUA(3,1)-p_g;
         QUA(4,1)=QUA(4,1)-p_g*u_s;
-    elseif strcmp(A_or_C,'A')~=1
-        error('NOT A(U) or C(U)!');
     end
 else
     error('NOT gas or solid in QUASILINEAR!');
