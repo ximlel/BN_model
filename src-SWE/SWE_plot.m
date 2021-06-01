93307375ffc7c148a2b9ffea63a64698de29d872
ximlel <qq352109085@gmail.com>
2021-06-01 08:31:38 +0800

    A

--- src-SWE/SWE_plot.m
+++ src-SWE/SWE_plot.m
@@ -3,25 +3,26 @@ u = 0.5*(u_L+u_R);
 
 N_MAX = 2500;
 d_xM = (x_max-x_min)/N_MAX;
-W_exact = zeros(N_MAX,2);
+W_exact = zeros(N_MAX+1,2);
 load ./steady_1.mat;
 W_exact(:,1)=h_E'+b_E';
 W_exact(:,2)=b_E';
 
 %plot
 col = '-m';
-%col = '+k';
+% col = '+k';
 % h=figure(1);
 % set(h,'position',[100 100 1150 750]);
 subplot(1,2,1);
 hold on
-plot(x_min:d_xM:x_max-d_xM,W_exact(:,1),'k','LineWidth',0.4);
+plot(x_min:d_xM:x_max,W_exact(:,1),'k','LineWidth',0.4);
 plot(x,h+Z_L(1:N),col,'MarkerSize',6);%col,'LineWidth',1.0);
+ylim([-0.1 2.1])
 plot(x,Z_L(1:N),col,'MarkerSize',6);%col,'LineWidth',1.0);
-plot(x_min:d_xM:x_max-d_xM,W_exact(:,2),'k','LineWidth',0.4);
+plot(x_min:d_xM:x_max,W_exact(:,2),'k','LineWidth',0.4);
 xlabel('Position','FontWeight','bold');
 ylabel('h+Z','FontWeight','bold');
-ylim([0 2])
+
 title('h+z');
 set(gca,'box','on');
 subplot(1,2,2);
--- src-SWE/root.m
+++ src-SWE/root.m
@@ -1,4 +1,4 @@
-N = 2500;
+N = 2501;
 x = linspace(0, 25, N);
 Z = @(x) (0.2-0.05*(x-10).^2).*(x>8 & x<12);
 b = Z(x)';
@@ -18,3 +18,21 @@ end
 b_E = b;
 h_E = h;
 save('steady_1.mat','b_E','h_E');
+
+
+p1 = b - 0.2 - 1.5*(1.53^2/9.81)^(1/3);
+p2 = 1.53^2/2/9.81;
+p = [ones(N,1) p1 zeros(N,1) p2*ones(N,1)];
+h = zeros(N,1);
+for i=1:N
+    r = roots(p(i,:));
+    if x(i)<10
+        h(i,1)=max(r);
+    else
+        [rr,pos]=sort(r);
+        h(i,1)=rr(2);
+    end
+end
+b_E = b;
+h_E = h;
+save('steady_2.mat','b_E','h_E');
